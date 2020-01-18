a700e125587fa58649a11a41c88fcbc1b419bf21
Rahul <kohli6010@gmail.com>
2020-01-18 21:51:43 +0530

    Changed Lorem to required description

--- index2.js
+++ index2.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const csv = require('csvtojson');
-const file = "./output.csv"
+const file = "./output1.csv"
 const publicDirectoryPath = path.join(__dirname, './public');
 app.use(express.static(publicDirectoryPath));
 
@@ -11,11 +11,12 @@ app.use('/', (req, res) => {
     res.sendFile(__dirname, '/public/feed.html');
 });
 
-app.get('/clicked', (req, res) => {
+app.get('/user1', (req, res) => {
 	csv()
 		.fromFile(file)
-		.then(data => { 
-			res.send(data)
+		.then(data => {
+			console.log(data[0]) 
+			res.send(data[0])
 		})
 });
 
