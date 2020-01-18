b66c58990208754a5cd986708f96dd1992f6a1ad
Rahul <kohli6010@gmail.com>
2020-01-18 13:30:04 +0530

    Converting CSV into JSON and taking to the frontend

--- index2.js
+++ index2.js
@@ -1,16 +1,22 @@
-const path=require('path')
-const express = require('express')
-const app = express()
-const port =process.env.PORT||3000
-const publicDirectoryPath=path.join(__dirname,'./public')
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const csv = require('csvtojson');
+const file = "./output.csv"
+const publicDirectoryPath = path.join(__dirname, './public');
+app.use(express.static(publicDirectoryPath));
 
-app.use(express.static(publicDirectoryPath))
+app.use('/', (req, res) => {
+    res.sendFile(__dirname, '/public/feed.html');
+});
 
-
-
-app.get('/', (req, res) => {
-	console.log(obj1);
-	res.json(obj1);
+app.get('/clicked', (req, res) => {
+	csv()
+		.fromFile(file)
+		.then(data => { 
+			res.send(data)
+		})
 });
 
 app.listen(5000, () => console.log(`server is running`));
