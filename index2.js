c93ff13d5f635c1387c5683424c79cc863df323c
Rahul <kohli6010@gmail.com>
2020-01-18 17:38:34 +0530

    Changed response to feed.html

--- index2.js
+++ index2.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const csv = require('csvtojson');
-const file = "./projectmetadata.csv"
+const file = "./output.csv"
 const publicDirectoryPath = path.join(__dirname, './public');
 app.use(express.static(publicDirectoryPath));
 
